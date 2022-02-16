0073a82b1f67e054dce858484297a4a2fe727c5e
yeenou <cye0328@naver.com>
2022-02-16 13:46:35 +0900

    location layout 수정

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -9,25 +9,25 @@ export default function Location(){
   const path = process.env.PUBLIC_URL;  
   const info = [
     {
-        title:"본점", 
-        latlng : new kakao.maps.LatLng(37.50711796614849,126.7564159502457),
-        imgSrc : path+'/img/marker1.png', 
-        imgSize : new kakao.maps.Size(232,99), 
-        imgPos : {offset: new kakao.maps.Point(116, 99)},
+      title:"본점", 
+      latlng : new kakao.maps.LatLng(37.50711796614849,126.7564159502457),
+      imgSrc : path+'/img/marker1.png', 
+      imgSize : new kakao.maps.Size(232,99), 
+      imgPos : {offset: new kakao.maps.Point(116, 99)},
     },
     {
-        title:"지점1", 
-        latlng : new kakao.maps.LatLng(33.450701, 126.570667),
-        imgSrc : path+'/img/marker2.png', 
-        imgSize : new kakao.maps.Size(232,99), 
-        imgPos : {offset: new kakao.maps.Point(116, 99)},
+      title:"지점1", 
+      latlng : new kakao.maps.LatLng(33.450701, 126.570667),
+      imgSrc : path+'/img/marker2.png', 
+      imgSize : new kakao.maps.Size(232,99), 
+      imgPos : {offset: new kakao.maps.Point(116, 99)},
     },
     {
-        title:"지점2", 
-        latlng : new kakao.maps.LatLng(37.557527,126.9222836),
-        imgSrc : path+'/img/marker3.png', 
-        imgSize : new kakao.maps.Size(232,99), 
-        imgPos : {offset: new kakao.maps.Point(116, 99)}, 
+      title:"지점2", 
+      latlng : new kakao.maps.LatLng(37.557527,126.9222836),
+      imgSrc : path+'/img/marker3.png', 
+      imgSize : new kakao.maps.Size(232,99), 
+      imgPos : {offset: new kakao.maps.Point(116, 99)}, 
     },
     {
       title:"지점3", 
@@ -79,12 +79,45 @@ export default function Location(){
     return ()=> window.removeEventListener('resize', mapSet);
   },[index]); 
 
+
   return (
     <main className="content location" ref={main}>
       <figure></figure>
       
       <div className="inner">
-        <h1>Location</h1>
+        {/* <span>say hi to the team</span> */}
+        <h1>Contact Us</h1>
+
+        <div className="wrap">
+          <article className="send">
+            <p>feel free to contact us and we will get back to you as soon as we can.</p>
+
+
+          </article>
+
+          <article className="oas">
+            <article>
+              <h2>opening hours</h2>
+              <p>Lorem ipsum sit.</p>
+              <p>Lorem ipsum sit.</p>
+              <p>Lorem ipsum sit.</p>
+              <p>Lorem ipsum sit.</p>
+            </article>
+            <article>
+              <h2>address</h2>
+              <p>Lorem ipsum dolor sit, <br />
+              amet consectur adipisicing <br />
+              doifelit.</p>
+            </article>
+            <article>
+              <h2>support</h2>
+              <p>Lorem ipsum dolor sit.</p>
+              <p>Lorem ipsum dolor sit.</p>
+            </article>
+          </article>
+        </div>
+        
+
         <section>
           <div id="map" ref={container}></div>
 
